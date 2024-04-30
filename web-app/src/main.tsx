5f31ed4d2da66128013b5edf8a1edf8a9821ef5e
Nikita Ablamskiy <shealtov@gmail.com>
2024-04-30 19:16:01 +0300

    initial templates fiLes for styles were added

--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM, { createRoot } from 'react-dom/client'
 import App from './app/App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "./index.css"
+
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
